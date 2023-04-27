3bb3cf997153edb0b0efb74884539281895603e0
rsdeviprasad <deviprasad.rs@pharmeasy.in>
2023-04-27 17:15:55 +0530

    add puzzle UI on the landing page directly

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import Puzzle from './puzzle';
+import ChessPuzzle from './chess-puzzle';
 
 export default function Home() {
   return (
     <div className={ styles.body }>
       <Head>
-        <title>SimpleChessMentor</title>
+        <title>Chessiest</title>
       </Head>
-      <Puzzle />
+      <ChessPuzzle />
     </div>
   );
 }
